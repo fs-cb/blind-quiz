d6ae989b4c90d1e3a8a1646dadc2cf2b31edbc9a
fs-cb <yukihide.shibita@gmail.com>
2026-04-29 14:44:52 +0900

    Update new.js

--- pages/admin/new.js
+++ pages/admin/new.js
@@ -163,7 +163,7 @@ export default function AdminNew() {
             <input
               type="text"
               className="input-field"
-              placeholder="例：都会のブラインドワイン会"
+              placeholder="例：ブラインドワイン会"
               value={title}
               onChange={e => setTitle(e.target.value)}
             />
@@ -194,7 +194,7 @@ export default function AdminNew() {
                   <input
                     type="text"
                     className="input-field"
-                    placeholder="例：産地、品種、年号"
+                    placeholder="例：産地、品種、VT"
                     value={item.name}
                     onChange={e => updateItem(itemIdx, 'name', e.target.value)}
                   />
